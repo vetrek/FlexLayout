bd19231387c930fbe525a93db27b9b5ff15f2c08
Valerio <listamaster@gmail.com>
2023-02-12 09:04:14 +0100

    replace FLEXLAYOUT_SWIFT_PACKAGE with SWIFT_PACKAGE

--- Package.swift
+++ Package.swift
@@ -18,15 +18,15 @@ let package = Package(
       dependencies: ["FlexLayoutYoga", "FlexLayoutYogaKit"],
       path: "Sources/Swift",
       cSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE")
+        .define("SWIFT_PACKAGE")
       ],
       cxxSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE"),
+        .define("SWIFT_PACKAGE"),
         .headerSearchPath("include/yoga/"),
         .headerSearchPath("./")
       ],
       swiftSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE")
+        .define("SWIFT_PACKAGE")
       ]
     ),
     .target(
@@ -34,10 +34,10 @@ let package = Package(
       dependencies: [],
       path: "Sources/yoga",
       cSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE")
+        .define("SWIFT_PACKAGE")
       ],
       cxxSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE"),
+        .define("SWIFT_PACKAGE"),
         .headerSearchPath("include/yoga/"),
         .headerSearchPath("./")
       ]
@@ -47,10 +47,10 @@ let package = Package(
       dependencies: ["FlexLayoutYoga"],
       path: "Sources/YogaKit",
       cSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE")
+        .define("SWIFT_PACKAGE")
       ],
       cxxSettings: [
-        .define("FLEXLAYOUT_SWIFT_PACKAGE"),
+        .define("SWIFT_PACKAGE"),
         .headerSearchPath("include/YogaKit/"),
         .headerSearchPath("./")
       ]
